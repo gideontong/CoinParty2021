49b83d5a8761a600b6eb7d5e0e2774edd30a9707
Gideon Tong <gideon@gideontong.com>
2021-01-14 13:26:19 -0800

    Prepare load and store functions

---
+++ app.py
@@ -0,0 +1,18 @@
+from json import load, dump
+
+secrets = None
+config = None
+
+def on_open():
+    with open('secrets.json') as secrets_file:
+        secrets = load(secrets_file)
+    with open('config.json') as config_file:
+        config = load(config_file)
+
+def on_exit():
+    with open('secrets.json', 'w') as secrets_file:
+        dump(secrets, secrets_file)
+
+on_open()
+
+on_exit()
