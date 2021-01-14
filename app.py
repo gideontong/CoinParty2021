8315056b80aff3df5e902fe67617ba3c377285ec
Gideon Tong <gideon@gideontong.com>
2021-01-14 13:54:18 -0800

    Separate project structure

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
 from json import load, dump
-from bitcash import Key as key
+from bitcash import Key
+from consolemenu import ConsoleMenu as Menu
+from consolemenu.items import FunctionItem as Option, MenuItem as SubMenu
+from project import GameMenus
 
 folder = 'config'
 secrets = None
 config = None
 
 def on_open():
+    global secrets, config
     with open(f'{folder}/secrets.json') as secrets_file:
         secrets = load(secrets_file)
     with open(f'{folder}/config.json') as config_file:
         config = load(config_file)
 
+def main():
+    global secrets, config
+    GameMenus(config, secrets)
+
 def on_exit():
+    global secrets
     with open(f'{folder}/secrets.json', 'w') as secrets_file:
         dump(secrets, secrets_file)
 
 on_open()
-
-import enquiries
-
-options = ['thing 1', 'thing 2', 'thing 3']
-choice = enquiries.choose('Choose one of these options: ', options)
-
-if enquiries.confirm('Do you want to write something?'):
-    text = enquiries.freetext('Write something interesting: ')
-    print(text)
 
 on_exit()
---
+++ project/__init__.py
@@ -0,0 +1,4 @@
+class GameMenus:
+    def __init__(self, config: dict, secrets: dict):
+        self.config = config
+        self.secrets = secrets
