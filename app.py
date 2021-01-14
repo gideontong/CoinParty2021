5a1773e9ca8df50d95f7095b6d405da78548a734
Gideon Tong <gideon@gideontong.com>
2021-01-14 13:56:53 -0800

    Pass variables instead of using globals

--- app.py
+++ app.py
@@ -3,25 +3,22 @@ from bitcash import Key
 from project import GameMenus
 
 folder = 'config'
-secrets = None
-config = None
 
 def on_open():
-    global secrets, config
     with open(f'{folder}/secrets.json') as secrets_file:
         secrets = load(secrets_file)
     with open(f'{folder}/config.json') as config_file:
         config = load(config_file)
+    return secrets, config
 
-def main():
-    global secrets, config
+def main(secrets, config):
     menus = GameMenus(config, secrets)
 
-def on_exit():
-    global secrets
+def on_exit(secrets):
     with open(f'{folder}/secrets.json', 'w') as secrets_file:
         dump(secrets, secrets_file)
 
-on_open()
-
-on_exit()
+if __name__ == '__main__':
+    secrets, config = on_open()
+    main(secrets, config)
+    on_exit(secrets)
